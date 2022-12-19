bf4ee37aa72d2dd28357300ca5ed2f22349a38c8
gleidsonteixeira <gleidsondeveloper@gmail.com>
2022-12-19 07:00:46 -0300

    corpo

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-
-
-function asideAnimation(){
+function asideAnimation()
+{
     document.addEventListener('scroll', (event) => {
         let altura = window.pageYOffset;
         if(altura > 50){
@@ -9,6 +8,34 @@ function asideAnimation(){
             document.getElementsByTagName("aside")[0].classList.remove("active");
         }
     });
+}
+
+function pill(position)
+{
+    let carrosel = document.querySelector(".carrosel");
+    let carroselPills = document.querySelectorAll(".pill");
+    let carroselItemWidth = document.querySelector(".carrosel li").clientWidth + 32;
+    
+    carroselPills.forEach( i => {
+        i.classList.remove("active");
+    })
+    
+    carrosel.scroll({
+        left: carroselItemWidth * position,
+        top: 0,
+        behavior: 'smooth'
+    });
+
+    event.target.classList.add("active");
+}
+
+function menu()
+{
+    if(document.getElementsByTagName("nav")[0].classList.contains("active")){
+        document.getElementsByTagName("nav")[0].classList.remove("active");
+        return;
+    }
+    document.getElementsByTagName("nav")[0].classList.add("active");
 }
 
 asideAnimation();
